c92f3ba0722a56e6ad6e2ba33b5d096e9af3c2f5
christopher_e <cjepps@gmail.com>
2021-06-04 14:48:10 -0700

    Added function for alpha value validation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-const guessList = document.querySelector("ul");
+const guessList = document.querySelector(".guessed-letters");
 // for the unordered list that will contain the guesses
-const guessButton = document.querySelector("button");
+const guessButton = document.querySelector(".guess");
 // button in use for pressing Guess
 const guessInput = document.querySelector(".letter");
 // used to select the input text element
 const wordInProgress = document.querySelector(".word-in-progress");
 const guessRemainShow = document.querySelector(".remaining");
-const guessRemainDisplay = document.querySelector("span");
-const letterGuessDisplay = document.querySelector(".message");
+const guessRemainDisplay = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia"; // default word to start with
+const guessedLetters = [];
 
 const placeholder = function(word) {
     const placeholderLetters = [];
@@ -24,7 +25,46 @@ placeholder(word);
 
 guessButton.addEventListener("click", function(e) { 
     e.preventDefault();
+    //clear the text here ^
+    message.innerText = "";
+
     const guess = guessInput.value;
-    console.log(guess);
+    //confirm letter value is singular
+    const goodGuess = inputAccept(guess);
+
+    if (goodGuess) {
+        makeGuess(guess);
+    }
     guessInput.value = "";
 });
+
+
+const inputAccept = function(input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        //check if value is empty
+        message.innerText = "Please enter a letter.";
+    } else if (input.length > 1) {
+        //is there more than one letter?
+        message.innerText = "Please enter only one letter.";
+    } else if (!input.match(acceptedLetter)) {
+        //reminder: use "!"" for checking for non-match value
+        message.innerText = "Please enter a letter from A to Z.";
+    } else {
+        //one letter received
+      return input;
+    }
+};
+
+const makeGuess = function(guess) {
+    guess = guess.toUpperCase();
+    //this converts values to uppercase
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that, try again.";
+        //this helps check for repeated guesses
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+        //this pushes the guesses to the array for tracking
+    }
+};
